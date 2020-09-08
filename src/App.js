6edcacbdd7272dc5545b1e54c335097cca8a86bf
GurpreetHans <gurpreet.hans26@gmail.com>
2020-09-08 11:04:25 +0530

    Form added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+import { withStyles } from '@material-ui/core/styles';
+import {Input, Button, FormControl} from '@material-ui/core';
 import './App.css';
 
-function App() {
-  const name = 'Gurpreet Singh'
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>{name}</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+})
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {data: {email: '', firstName: '', lastName: '', occupation: ''} };
+  }
+
+  submitForm = () => {
+    $('form#user input').bind('onchange', function (e){
+      field = e.attr('name')
+      value = e.value()
+      this.setState(..., {field: value})
+    })
+  }
+
+  render() {
+
+    const { classes } = this.props;
+    return (
+      <div className="App">
+        <form id="user" className={classes.root} noValidate autoComplete="off">
+          <FormControl>
+            <Input placeholder="First Name" inputProps={{ 'aria-label': 'description' }} />
+          </FormControl>
+          <FormControl>
+          <Input placeholder="Last Name" inputProps={{ 'aria-label': 'description' }} />
+          </FormControl>
+          <FormControl>
+          <Input placeholder="Email" inputProps={{ 'aria-label': 'description' }} />
+          </FormControl>
+          <FormControl>
+          <Input placeholder="Occupation" inputProps={{ 'aria-label': 'description' }} />
+          </FormControl>
+          <FormControl>
+            <Button variant="contained" color="primary" disableElevation onClick={() => this.submitForm()}>
+              Submit
+            </Button>
+          </FormControl>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withStyles(useStyles)(App)
